f5e23ae700d69af558f51a8611432cc681afa3b8
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-26 16:54:11 -0300

    feat: create Read for user

--- gRPC_server/user/user.go
+++ gRPC_server/user/user.go
@@ -10,6 +10,7 @@ import (
 	pb "github.com/aaguero96/Klever-Desafio-Tecnico/pb/user"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
@@ -47,6 +48,41 @@ func (s UserServer) Create(ctx context.Context, in *pb.NewUser) (*pb.User, error
 		Email:    in.GetEmail(),
 		Password: in.GetPassword(),
 	}, nil
+}
+
+func (s UserServer) Read(ctx context.Context, in *pb.Filter) (*pb.Users, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return &pb.Users{}, err
+	}
+
+	userCollection := db.Collection("users")
+
+	filter := bson.D{{}}
+	filter = append(filter, bson.E{
+		Key: "name",
+		Value: bson.D{
+			{"$regex", primitive.Regex{Pattern: "^" + in.GetName() + ".*", Options: "i"}},
+		},
+	})
+
+	cur, err := userCollection.Find(context.TODO(), filter, options.Find())
+	if err != nil {
+		return &pb.Users{}, err
+	}
+	defer cur.Close(context.TODO())
+
+	var result []*pb.User
+
+	for cur.Next(context.TODO()) {
+		user := pb.User{}
+		if err = cur.Decode(&user); err != nil {
+			return &pb.Users{}, err
+		}
+		result = append(result, &user)
+	}
+
+	return &pb.Users{Users: result}, nil
 }
 
 func UserService(s grpc.ServiceRegistrar, lis net.Listener) {
