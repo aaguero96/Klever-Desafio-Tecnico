48e36e0819e22ee731ce69ee59f510a305a3944f
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-26 16:54:56 -0300

    feat: apply Read in user controller

--- api/controllers/user.controller.go
+++ api/controllers/user.controller.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/aaguero96/Klever-Desafio-Tecnico/api/models"
@@ -46,4 +47,26 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	user.ID = response.UserId
 	responses.JSON(w, http.StatusCreated, user)
+}
+
+func Read(w http.ResponseWriter, r *http.Request) {
+	name := strings.ToLower(r.URL.Query().Get("userName"))
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer conn.Close()
+
+	c := pb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := c.Read(ctx, &pb.Filter{
+		Name: name,
+	})
+
+	responses.JSON(w, http.StatusCreated, response.Users)
 }
