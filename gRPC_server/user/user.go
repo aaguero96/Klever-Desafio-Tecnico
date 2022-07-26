b7b8d4b3338666f4e8e7275be36dd2afbd7d4802
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-26 13:36:58 -0300

    feat:  implement Create user

--- gRPC_server/user/user.go
+++ gRPC_server/user/user.go
@@ -2,11 +2,14 @@ package user_server
 
 import (
 	"context"
+	"errors"
 	"log"
-	"math/rand"
 	"net"
 
+	"github.com/aaguero96/Klever-Desafio-Tecnico/api/database"
 	pb "github.com/aaguero96/Klever-Desafio-Tecnico/pb/user"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc"
 )
 
@@ -14,11 +17,32 @@ type UserServer struct {
 	pb.UnimplementedUserServiceServer
 }
 
-func (s *UserServer) Create(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
-	log.Printf("Recieved: %v", in.GetName())
-	var user_id uint32 = uint32(rand.Intn(1000))
+func (s UserServer) Create(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return &pb.User{}, err
+	}
+
+	userCollection := db.Collection("users")
+
+	newUser := bson.D{
+		{"name", in.GetName()},
+		{"email", in.GetEmail()},
+		{"password", in.GetPassword()},
+	}
+
+	result, err := userCollection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		return &pb.User{}, err
+	}
+
+	oid, ok := result.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return &pb.User{}, errors.New("Error after transform id in string")
+	}
+
 	return &pb.User{
-		UserId:   user_id,
+		UserId:   oid.Hex(),
 		Name:     in.GetName(),
 		Email:    in.GetEmail(),
 		Password: in.GetPassword(),
