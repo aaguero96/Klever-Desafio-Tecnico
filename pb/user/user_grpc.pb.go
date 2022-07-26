74f5080b2e9c2b686b1fc5e448ac9092f7982884
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-26 16:58:50 -0300

    build: user pb

--- pb/user/user_grpc.pb.go
+++ pb/user/user_grpc.pb.go
@@ -24,6 +24,7 @@ const _ = grpc.SupportPackageIsVersion7
 type UserServiceClient interface {
 	Create(ctx context.Context, in *NewUser, opts ...grpc.CallOption) (*User, error)
 	Read(ctx context.Context, in *Filter, opts ...grpc.CallOption) (*Users, error)
+	ReadById(ctx context.Context, in *UserId, opts ...grpc.CallOption) (*User, error)
 }
 
 type userServiceClient struct {
@@ -52,12 +53,22 @@ func (c *userServiceClient) Read(ctx context.Context, in *Filter, opts ...grpc.C
 	return out, nil
 }
 
+func (c *userServiceClient) ReadById(ctx context.Context, in *UserId, opts ...grpc.CallOption) (*User, error) {
+	out := new(User)
+	err := c.cc.Invoke(ctx, "/UserService/ReadById", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility
 type UserServiceServer interface {
 	Create(context.Context, *NewUser) (*User, error)
 	Read(context.Context, *Filter) (*Users, error)
+	ReadById(context.Context, *UserId) (*User, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -70,6 +81,9 @@ func (UnimplementedUserServiceServer) Create(context.Context, *NewUser) (*User, 
 }
 func (UnimplementedUserServiceServer) Read(context.Context, *Filter) (*Users, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Read not implemented")
+}
+func (UnimplementedUserServiceServer) ReadById(context.Context, *UserId) (*User, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ReadById not implemented")
 }
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 
@@ -120,6 +134,24 @@ func _UserService_Read_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_ReadById_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UserId)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).ReadById(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/UserService/ReadById",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).ReadById(ctx, req.(*UserId))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -134,6 +166,10 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Read",
 			Handler:    _UserService_Read_Handler,
+		},
+		{
+			MethodName: "ReadById",
+			Handler:    _UserService_ReadById_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
