b6cb27a028163bd23409bd9a81346da7a48b4b7b
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-26 13:34:06 -0300

    feat: create connection with database

---
+++ api/database/database.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func Connect() (*mongo.Database, error) {
+	client, err := mongo.Connect(
+		context.TODO(),
+		options.Client().ApplyURI("mongodb://localhost:27017"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		return nil, err
+	}
+
+	db := client.Database("klever")
+
+	return db, err
+}
