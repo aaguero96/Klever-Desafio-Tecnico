3635b0281bce55bab6f567fc50fde8a5b28080d5
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-27 08:34:04 -0300

    feat: add Create for service server

--- gRPC_server/service/service.go
+++ gRPC_server/service/service.go
@@ -10,6 +10,7 @@ import (
 	pb "github.com/aaguero96/Klever-Desafio-Tecnico/pb/service"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
@@ -45,6 +46,52 @@ func (s ServiceServer) Create(ctx context.Context, in *pb.NewService) (*pb.Servi
 		Name:      in.GetName(),
 		Site:      in.GetSite(),
 	}, nil
+}
+
+func (s ServiceServer) Read(ctx context.Context, in *pb.FilterService) (*pb.Services, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return &pb.Services{}, err
+	}
+
+	userCollection := db.Collection("services")
+
+	filter := bson.D{{}}
+	filter = append(filter, bson.E{
+		Key: "name",
+		Value: bson.M{
+			"$regex": primitive.Regex{Pattern: "^" + in.GetName() + ".*", Options: "i"},
+		},
+	})
+
+	cur, err := userCollection.Find(context.TODO(), filter, options.Find())
+	if err != nil {
+		return &pb.Services{}, err
+	}
+	defer cur.Close(context.TODO())
+
+	var result []*pb.Service
+
+	for cur.Next(context.TODO()) {
+		type DecodedUser struct {
+			ObjectID primitive.ObjectID `bson:"_id"`
+			Name     string
+			Site     string
+		}
+		var decodedUser DecodedUser
+
+		if err = cur.Decode(&decodedUser); err != nil {
+			return &pb.Services{}, err
+		}
+		user := pb.Service{
+			Name:      decodedUser.Name,
+			Site:      decodedUser.Site,
+			ServiceId: decodedUser.ObjectID.Hex(),
+		}
+		result = append(result, &user)
+	}
+
+	return &pb.Services{Services: result}, nil
 }
 
 func ServiceService(s grpc.ServiceRegistrar, lis net.Listener) {
