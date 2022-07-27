0e5abd1f532076a89e8ba0dffb2877f8199f2535
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-27 11:27:26 -0300

    feat: apply Read to upvote controller feat: apply ReadById to upvote controller feat: apply Update to upvote controller

--- api/controllers/upvote/upvote.controller.go
+++ api/controllers/upvote/upvote.controller.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/aaguero96/Klever-Desafio-Tecnico/api/models"
@@ -48,6 +49,89 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	upvote.ID = response.UpvoteId
 	responses.JSON(w, http.StatusCreated, upvote)
+}
+
+func Read(w http.ResponseWriter, r *http.Request) {
+	typeUpvote := strings.ToLower(r.URL.Query().Get("userName"))
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer conn.Close()
+
+	c := pb.NewUpvoteServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := c.Read(ctx, &pb.FilterUpvote{
+		Type: typeUpvote,
+	})
+
+	responses.JSON(w, http.StatusCreated, response.Upvotes)
+}
+
+func ReadById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	upvoteId := params["upvoteId"]
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer conn.Close()
+
+	c := pb.NewUpvoteServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := c.ReadById(ctx, &pb.UpvoteId{
+		UpvoteId: upvoteId,
+	})
+
+	responses.JSON(w, http.StatusCreated, response)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var upvote models.Upvote
+	if err = json.Unmarshal(body, &upvote); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	params := mux.Vars(r)
+
+	upvoteId := params["upvoteId"]
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer conn.Close()
+
+	c := pb.NewUpvoteServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err = c.Update(ctx, &pb.Upvote{
+		UpvoteId:  upvoteId,
+		ServiceId: upvote.ServiceID,
+	})
+
+	responses.JSON(w, http.StatusNoContent, nil)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
