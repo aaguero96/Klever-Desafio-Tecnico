de5174f9870118a3292a1614ef1d72870c0fd5ac
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-27 11:00:41 -0300

    feat: apply Delete to upvote controller

--- api/controllers/upvote/upvote.controller.go
+++ api/controllers/upvote/upvote.controller.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aaguero96/Klever-Desafio-Tecnico/api/models"
 	"github.com/aaguero96/Klever-Desafio-Tecnico/api/responses"
 	pb "github.com/aaguero96/Klever-Desafio-Tecnico/pb/upvote"
+	"github.com/gorilla/mux"
 	"google.golang.org/grpc"
 )
 
@@ -47,4 +48,28 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	upvote.ID = response.UpvoteId
 	responses.JSON(w, http.StatusCreated, upvote)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	upvoteId := params["upvoteId"]
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer conn.Close()
+
+	c := pb.NewUpvoteServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err = c.Delete(ctx, &pb.UpvoteId{
+		UpvoteId: upvoteId,
+	})
+
+	responses.JSON(w, http.StatusNoContent, nil)
 }
