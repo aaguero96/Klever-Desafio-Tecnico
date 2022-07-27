bbd506c2f6f689f4850a80a7e91202e2a20ae341
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-27 10:53:01 -0300

    refactor: change bson names

--- api/models/Upvote.model.go
+++ api/models/Upvote.model.go
@@ -2,8 +2,8 @@ package models
 
 type Upvote struct {
 	ID        string `bson:"_id,omitempty"`
-	ServiceID string `bson:"service_id,omitempty"`
-	UserID    string `bson:"user_id,omitempty"`
+	ServiceID string `bson:"serviceId,omitempty"`
+	UserID    string `bson:"userId,omitempty"`
 	Vote      string `bson:"vote,omitempty"`
 	Comment   string `bson:"comment,omitempty"`
 }
