0890fa2527c83699148aaf8025c475d783c90d8b
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-28 15:24:18 -0300

    feat: apply LoadEnv

--- gRPC_server/main.go
+++ gRPC_server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
+	"github.com/aaguero96/Klever-Desafio-Tecnico/config"
 	service_server "github.com/aaguero96/Klever-Desafio-Tecnico/gRPC_server/service"
 	upvote_server "github.com/aaguero96/Klever-Desafio-Tecnico/gRPC_server/upvote"
 	user_server "github.com/aaguero96/Klever-Desafio-Tecnico/gRPC_server/user"
@@ -11,7 +13,10 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":50052")
+	config.LoadEnv()
+
+	address := fmt.Sprintf(":%d", config.Port)
+	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,6 +27,7 @@ func main() {
 	service_server.ServiceService(s, lis)
 	upvote_server.UpvoteService(s, lis)
 
+	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
