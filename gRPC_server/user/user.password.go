a0645034cf20f5b488a5953b239daca9f54735f4
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-28 15:34:31 -0300

    feat: create hashPassword

---
+++ gRPC_server/user/user.password.go
@@ -0,0 +1,15 @@
+package user_server
+
+import "golang.org/x/crypto/bcrypt"
+
+var (
+	COST_PASSWORD = 14
+)
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), COST_PASSWORD)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
