d855162478ba73a876ead5f1040b4b1e08d98962
aaguero96 <andre.luiz_1996@hotmail.com>
2022-07-28 12:00:55 -0300

    feat: add validate name

---
+++ gRPC_server/user/user.validations.go
@@ -0,0 +1,14 @@
+package user_server
+
+import (
+	"errors"
+	"log"
+)
+
+func validateName(name string) error {
+	if name == "" {
+		log.Println("Name is required")
+		return errors.New("Name is required")
+	}
+	return nil
+}
